2301225562a39f71352dbb26175400515b192e07
wolfitis <webblog123@gmail.com>
2021-12-13 11:45:58 -0500

    add basic routes-navigation

---
+++ frontend-js/README.md
@@ -0,0 +1,1 @@
+Thanks to: [react-router tutorial](https://github.com/remix-run/react-router/blob/main/docs/getting-started/tutorial.md)
--- frontend-js/src/main.jsx
+++ frontend-js/src/main.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from './routes/invoice';
 
+// From React docs:
+// StrictMode is a tool for highlighting potential problems in an application. Like Fragment, StrictMode does not render any visible UI. It activates additional checks and warnings for its descendants
+// Note: Strict mode checks are run in development mode only; they do not impact the production build
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />}> 
+          <Route path=':invoiceId' element={<Invoice />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Empty Page!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
+
+// This will work just fine
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root')
+// )
